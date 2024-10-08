e974ac15d5d7f8f16319870629dd473df97bc33c
syednoor058 <syednoor058@gmail.com>
2024-10-08 11:27:36 +0600

    Services sml device scale off.

--- src/components/services/ServiceCard.jsx
+++ src/components/services/ServiceCard.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 export default function ServiceCard(props) {
   return (
-    <div className="flex flex-col gap-2 md:gap-3 p-3 sml:p-3 md:p-5 lg:hover:border bg-gradient-to-br from-primaryColor1 to-accentColor1 hover:bg-gradient-to-t hover:from-darkBG1 hover:to-darkBG1 rounded-md hover:scale-[1.05] duration-[400ms] shadow-xl cursor-pointer group justify-center ">
+    <div className="flex flex-col gap-2 md:gap-3 p-3 sml:p-3 md:p-5 lg:hover:border bg-gradient-to-br from-primaryColor1 to-accentColor1 hover:bg-gradient-to-t hover:from-darkBG1 hover:to-darkBG1 rounded-md md:hover:scale-[1.05] duration-[400ms] shadow-xl cursor-pointer group justify-center ">
       <div className="flex text-xl sml:text-3xl mdl:text-3xl lg:text-3xl text-yellow-200 group-hover:text-accentColor1">
         {props.icon}
       </div>
